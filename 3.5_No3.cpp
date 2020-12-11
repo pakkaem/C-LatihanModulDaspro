ce4e700dec3a2fe05918d6fa00d69fd93db03e6d
pakkaem <70743514+pakkaem@users.noreply.github.com>
2020-12-11 18:14:07 +0700

    Update 3.5_No3.cpp

--- 3.5_No3.cpp
+++ 3.5_No3.cpp
@@ -8,9 +8,10 @@ int main (){
     int jumlah, total;
 
     cout<<"========================================="<<endl;
-    cout<<"     Program Menghitung Harga Barang     "<<endl;
+    cout<<"|    Program Menghitung Harga Barang    |"<<endl;
     cout<<"========================================="<<endl;
     cout<<"|   Jumlah Barang   ||   Harga Barang   |"<<endl;
+    cout<<"========================================="<<endl;
     cout<<"|       < 100       ||   Rp 9.999,00    |"<<endl;
     cout<<"|      >= 100       ||   Rp 7.490,00    |"<<endl;
     cout<<"|      >= 200       ||   Rp 5.999,00    |"<<endl;
